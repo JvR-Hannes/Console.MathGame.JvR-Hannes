35f16e87756ef9b0eacc5c92acbbc98c470e1c3a
Hannes Jansen van Rensburg <jpijansenvanrensburg@gmail.com>
2024-08-12 10:44:42 +0200

    Updated Stopwatch to show only 1 decimal Cleaned up code

--- Console.MathGame.JvR-Hannes/Program.cs
+++ Console.MathGame.JvR-Hannes/Program.cs
@@ -2,6 +2,8 @@
 
 var date = DateTime.UtcNow;
 
+var games = new List<string>();
+
 string name = GetName();
 
 Menu(name);
@@ -11,50 +13,60 @@ string GetName()
     Console.WriteLine("Please type your name");
 
     var name = Console.ReadLine();
-    return name;
+    return name.ToUpper();
 }
 
 void Menu(string name)
 {
     Console.WriteLine("----------------------------------------");
-    Console.WriteLine($"Hello {name.ToUpper()}. It's {date.DayOfWeek}. This is your math's game. It's great that you're working on improving yourself\n");
-    Console.WriteLine(@$"What game would you like to play today? Choose from the options below:
-A - Addition
-S - Subtraction
-M - Multiplication
-D - Division
-R - Random Game
-Q - Quit the Program");
-    Console.WriteLine("----------------------------------------");
+    Console.WriteLine($"Hello {name}. It's {date.DayOfWeek}. This is your math's game. It's great that you're working on improving yourself\n");
 
-    var gameSelected = Console.ReadLine();
+    var isGameOn = true;
 
-    switch (gameSelected.Trim().ToLower())
+    do
     {
-        case "a":
-            AdditionGame("Addition game selected");
-            break;
-        case "s":
-            SubtractionGame("Subtraction game selected");
-            break;
-        case "m":
-            MultiplicationGame("Multiplication game selected");
-            break;
-        case "d":
-            DivisionGame("Division game selected");
-            break;
-        case "r":
-            RandomGame("Random game selected");
-            break;
-        case "q":
-            Console.WriteLine("Goodbye!");
-            Environment.Exit(1);
-            break;
-        default:
-            Console.WriteLine("Invalid Input!");
-            Environment.Exit(1);
-            break;
-    }
+        Console.Clear();
+        Console.WriteLine(@$"What game would you like to play today? Choose from the options below:
+
+    A - Addition
+    S - Subtraction
+    M - Multiplication
+    D - Division
+    R - Random Game
+    Q - Quit the Program");
+
+        Console.WriteLine("----------------------------------------");
+
+        var gameSelected = Console.ReadLine();
+
+        switch (gameSelected.Trim().ToLower())
+        {
+            case "a":
+                AdditionGame("Addition game");
+                break;
+            case "s":
+                SubtractionGame("Subtraction game");
+                break;
+            case "m":
+                MultiplicationGame("Multiplication game");
+                break;
+            case "d":
+                DivisionGame("Division game");
+                break;
+            case "r":
+                RandomGame("Random game");
+                break;
+            case "q":
+                Console.WriteLine("Goodbye!");
+                isGameOn = false;
+                Environment.Exit(1);
+                break;
+            default:
+                Console.WriteLine("Invalid Input!");
+                Environment.Exit(1);
+                break;
+        }
+    } while (isGameOn);
 }
 
 void RandomGame(string message)
@@ -65,6 +77,7 @@ void RandomGame(string message)
 
 void DivisionGame(string message)
 {
+    Console.Clear();
     Console.WriteLine(message);
 
     var score = 0;
@@ -102,15 +115,21 @@ void DivisionGame(string message)
             Console.ReadLine();
         }
 
+        TimeSpan ts = stopwatch.Elapsed;
+        string elapsedTime = string.Format("{0:0.0}", ts.TotalSeconds);
+
         if (i == numberOfQuestions - 1)
         {
-            Console.WriteLine($"Game Over. You final score is {score}/{numberOfQuestions}. You took {stopwatch.Elapsed.TotalSeconds} seconds to answer the question.");
+            Console.WriteLine($"Game Over. You final score is {score}/{numberOfQuestions}. You took {elapsedTime} seconds to answer the question.");
+            Console.WriteLine("Press any key to return to the menu");
+            Console.ReadLine();
         }
     }
 }
 
 void MultiplicationGame(string message)
 {
+    Console.Clear();
     Console.WriteLine(message);
 
     var random = new Random();
@@ -151,15 +170,21 @@ void MultiplicationGame(string message)
             Console.ReadLine();
         }
 
+        TimeSpan ts = stopwatch.Elapsed;
+        string elapsedTime = string.Format("{0:0.0}", ts.TotalSeconds);
+
         if (i == numberOfQuestions - 1)
         {
-            Console.WriteLine($"Game Over. You final score is {score}/{numberOfQuestions}. You took {stopwatch.Elapsed.TotalSeconds} seconds to answer the question.");
+            Console.WriteLine($"Game Over. You final score is {score}/{numberOfQuestions}. You took {elapsedTime} seconds to answer the question.");
+            Console.WriteLine("Press any key to return to the menu");
+            Console.ReadLine();
         }
     }
 }
 
 void SubtractionGame(string message)
 {
+    Console.Clear();
     Console.WriteLine(message);
 
     var random = new Random();
@@ -200,19 +225,26 @@ void SubtractionGame(string message)
             Console.ReadLine();
         }
 
+        TimeSpan ts = stopwatch.Elapsed;
+        string elapsedTime = string.Format("{0:0.0}", ts.TotalSeconds);
+
         if (i == numberOfQuestions - 1)
         {
-            Console.WriteLine($"Game Over. You final score is {score}/{numberOfQuestions}. You took {stopwatch.Elapsed.TotalSeconds} seconds to answer the question.");
+            Console.WriteLine($"Game Over. You final score is {score}/{numberOfQuestions}. You took {elapsedTime} seconds to answer the question.");
+            Console.WriteLine("Press any key to return to the menu");
+            Console.ReadLine();
         }
     }
 }
 
 void AdditionGame(string message)
 {
+    Console.Clear();
     Console.WriteLine(message);
 
     var random = new Random();
     var score = 0;
+    // Implementing the Stopwatch function to time the user on the questions asked
     Stopwatch stopwatch = new Stopwatch();
 
     // Requesting the number of questions from the user
@@ -250,11 +282,19 @@ void AdditionGame(string message)
             Console.ReadLine();
         }
 
+        // Updating Stopwatch to show only 1 decimals
+        TimeSpan ts = stopwatch.Elapsed;
+        string elapsedTime = string.Format("{0:0.0}", ts.TotalSeconds);
+
         if (i == numberOfQuestions - 1) 
         {
-            Console.WriteLine($"Game Over. You final score is {score}/{numberOfQuestions}. You took {stopwatch.Elapsed.TotalSeconds} seconds to answer the question.");
+            Console.WriteLine($"Game Over. You final score is {score}/{numberOfQuestions}. You took {elapsedTime} seconds to answer the question.");
+            Console.WriteLine("Press any key to return to the menu");
+            Console.ReadLine();
         }
     }
+
+    games.Add("");
 
 }
 
